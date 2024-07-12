2e872095322f78894c121a1d70236ea86c1fb6a1
Daniel <youcheng28564@gmail.com>
2024-07-12 16:32:41 +0800

    0712 試寫價格查詢

--- src/main/java/sellphone/product/model/ProductRepository.java
+++ src/main/java/sellphone/product/model/ProductRepository.java
@@ -1,11 +1,15 @@
 package sellphone.product.model;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
-	
-	
-	
+	@Query("FROM Product p WHERE p.price BETWEEN :minPrice AND :maxPrice")
+    List<Product> findByPriceRange(@Param("minPrice") Integer minPrice, @Param("maxPrice") Integer maxPrice);
 }
