d65351be0ecb03a904b9a11dfa250740364cf405
x4580628 <x4580628@gmail.com>
2024-07-14 01:50:17 +0800

    新增Dashboard

--- src/main/java/sellphone/orders/model/Dashboard.java
+++ src/main/java/sellphone/orders/model/Dashboard.java
@@ -1,5 +1,23 @@
 package sellphone.orders.model;
 
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@Entity
 public class Dashboard {
-    //test123456
+    @Id
+    private String yyyymm;
+    private BigDecimal totalAmount;
+    private BigDecimal lmMonthAmount;
+    private BigDecimal lyYearAmount;
+    private BigDecimal monthlyGrate;
+    private BigDecimal yearlyGrate;
+    private int orderCount;
+    private int lmOrderCount;
+    private int lyOrderCount;
+    private BigDecimal monthlyCountGrate;
+    private BigDecimal yearlyCountGrate;
 }
