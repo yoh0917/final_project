8d64fb63925b0382e06760f07edfaf050385dcf6
rayliu1016 <ds31013101@gmail.com>
2024-07-16 11:20:53 +0800

    test

--- src/main/java/sellphone/forum/model/CommentRepository.java
+++ src/main/java/sellphone/forum/model/CommentRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findByPostId(Integer postId);
+    List<Comment> findBypostId(Integer postId);
 }
