941a04beb0a16c0f3f5c47c8570f52f694b37a41
leo <leo312654@gmail.com>
2024-07-23 16:43:29 +0800

    adding spring.aop into project

--- src/main/java/sellphone/user/aop/UserLogAspect.java
+++ src/main/java/sellphone/user/aop/UserLogAspect.java
@@ -1,6 +1,37 @@
 package sellphone.user.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class UserLogAspect {
+	
+	@Pointcut("execution(* sellphone.user.controller.DashBoardFilter.checkLogin(..))")
+    public void dashBoardAcessPointCut() {
+    }
+	
+	@Around("dashBoardAcessPointCut()")
+	public Object dashboardAcessAround(ProceedingJoinPoint pjp) throws Throwable {
+		
+		System.out.println("before login");
+		Object obj = pjp.proceed();
+		System.out.println("After login");
+		
+		return obj;
+	}
 
+	//	@Around("execution(* sellphone.user.controller.UserController.checkLogin(..))")
+//	public Object userLoginAround(ProceedingJoinPoint pjp) throws Throwable {
+//		
+//		System.out.println("before login");
+//		Object obj = pjp.proceed();
+//		System.out.println("After login");
+//		
+//		return obj;
+//	}
+	
 }
