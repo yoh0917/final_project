5bbd5dcf837fe2b807936527293a87f8bc433ee1
Daniel <youcheng28564@gmail.com>
2024-07-29 14:35:08 +0800

    前台熱銷排行

--- src/main/java/sellphone/orders/repository/OrderDetailRepository.java
+++ src/main/java/sellphone/orders/repository/OrderDetailRepository.java
@@ -2,9 +2,14 @@ package sellphone.orders.repository;
 
 import sellphone.orders.model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
     List<OrderDetail> findByOrderId(String orderId);
+    
+    
+    @Query(value = "SELECT TOP 4 PRODUCTID,  COUNT(*) FROM O0002_ORDERDETAIL GROUP BY PRODUCTID ORDER BY 2 DESC ",nativeQuery = true)
+    List<Object[]> getMostSale();
 }
